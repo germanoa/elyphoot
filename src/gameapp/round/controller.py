ea6f4c4ec84e43006cd373b8372dab7cbd6d838f
raphaelbaldi@gmail.com <raphaelbaldi@gmail.com@cf4997be-b4d0-2e88-bcf9-847bdaf59e33>
2012-06-16 03:47:07 +0000

    - Tested a full season running round by round! - Fix: season is now marked as completed when the last round ends. - Fix: season.current_round is updated every time a round ends and is set to None when the last round ends. - Fix: season.winner is correctly set now (gameapp.season.controller.get_team_table(season, 1)[0]) when season ends. - Fix: there were still some references to round inside round/controller.py. round is a reserved word inside python.

--- src/gameapp/round/controller.py
+++ src/gameapp/round/controller.py
@@ -1,7 +1,8 @@
 import gameapp.match.controller
+import gameapp.season.controller
 from gameapp.models import Round
 
-def run_round(game_round):
+def run_round(season, game_round):
     if game_round is None or game_round.resolved:
         return False
         
@@ -14,16 +15,16 @@ def run_round(game_round):
     game_round.resolved = True
     game_round.save()
     
-    if game_round.current_season is not None:
-        if game_round.round_number < 14:
-            game_round.current_season.completed = True
-            game_round.current_season.save()
-            game_round.current_season = None
-            game_round.save()
+    if season is not None:
+        if game_round.round_number >= 14:
+            season.completed = True
+            season.current_round = None
+            season.winner = gameapp.season.controller.get_team_table(season, 1)[0]
         else:
-            next_round = game_round.current_season.rounds.get(round_number=(game_round.round_number + 1))
-            next_round.current_season = game_round.current_season
-            next_round.save()
+            next_round = game_round.round_number + 1
+            season.current_round = season.rounds.get(round_number=next_round)
+    
+        season.save()
     
     return False # todos os matches resolvidos
     
@@ -42,10 +43,10 @@ def create_rounds(season):
             if game_round is not None:
                 game_round.save()
         
-            game_round = Round(round_number=round_count,\
+            game_round = Round(round_number=round_count, \
                            resolved=False)
             game_round.save()
-            season.rounds.add(round)
+            season.rounds.add(game_round)
             round_count += 1
         
         game_round.matches.add(matches_serie_a[match_count])
